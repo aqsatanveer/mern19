6904e95e9679d9eddc1a4069d28bf55f5eb6f4a5
aqsatanveer <aqsatanveer949@gmail.com>
2024-07-29 01:39:41 -0700

    29/07/24

---
+++ usercontroller.js
@@ -0,0 +1,16 @@
+const usercontroller = {
+  addUser: () => {
+    console.log("create user Function");
+  },
+
+  getUser: () => {
+    console.log("get user");
+  },
+  updateUser: () => {
+    console.log("update user");
+  },
+  dellUser: () => {
+    console.log("delete user");
+  },
+};
+module.exports = usercontroller;
